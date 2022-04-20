af4a61db08e2c262735cb8f3237914fc4a458417
miriik <miriik@ttu.ee>
2022-04-21 00:00:31 +0300

    Readd cors middleware.

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,13 @@ import identityRouter from './routes/identity';
 import blogCommentRouter from './routes/blogComments';
 import { MongoClient } from 'mongodb';
 import feedbackRouter from './routes/feedback';
+import cors from "cors"
 
 export const client = new MongoClient(process.env.DB_CONN_STRING);
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/blogPost', blogPostRouter);
